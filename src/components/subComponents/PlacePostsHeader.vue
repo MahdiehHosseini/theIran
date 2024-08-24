34f7c53ed99dc19b0db903fc93d99dd8ee0dd923
MahdiehHosseini <mahdieh1384.sh@gmail.com>
2024-08-24 13:45:09 +0330

    update places location

--- src/components/subComponents/PlacePostsHeader.vue
+++ src/components/subComponents/PlacePostsHeader.vue
@@ -2,8 +2,8 @@
     <header class="lg:flex lg:flex-col lg:items-center">
       <div class="text-white h-96 mx-14 lg:w-1/2 bg-black box-border relative rounded-2xl mt-16" :style="{background: `url('${place.imgUrl}')` ,backgroundRepeat: 'noRepeat',backgroundPosition: 'center', backgroundSize: 'cover'}">
         <div class="animate__fadeInDown animate__animated absolute bottom-0  m-5 backdrop-brightness-50 p-3 rounded-lg">
-            <h1 class="text-4xl">{{ place.name }}</h1>
-            <p class="text-neutral-200 text-2xl">{{place.location}}</p>
+            <h1 class="text-4xl">{{ place?.name }}</h1>
+            <p class="text-neutral-200 text-2xl">{{ place?.location && `${place?.location[0]}, ${place?.location[1]}` }}</p>
         </div>
       </div>
     </header>
